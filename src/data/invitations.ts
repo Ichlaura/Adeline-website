3af59acb87794a444d748ad2b9ac97ebb480475e
laura nunez <laupao41@gmail.com>
2026-07-08 11:29:47 +0900

    arreglo de paises y se agergo usa y peru y japon en invitation.ts

--- src/data/invitations.ts
+++ src/data/invitations.ts
@@ -1,7 +1,9 @@
+export type CountryCode = "co" | "pe" | "us" | "jp";
+
 export type Invitation = {
   title: string;
   category: string;
-  country: "co" | "pe" | "us" | "jp";
+  country: CountryCode;
   price: string;
   closedImage: string;
   openImage: string;
@@ -9,348 +11,426 @@ export type Invitation = {
 
 export const invitations: Invitation[] = [
   // ==========================
-  // QUINCE AÑOS
+  // COLOMBIA
   // ==========================
 
   {
     title: "Emerald Quinceañera",
     category: "Quince Años",
-      country: "co",
+    country: "co",
     price: "$49",
     closedImage: "/quinceanosverde_close.jpeg",
     openImage: "/quinceanosverde_open.jpeg",
   },
-
-  
   {
     title: "Royal Blue Quinceañera",
     category: "Quince Años",
-      country: "co",
+    country: "co",
     price: "$49",
     closedImage: "/quinceanosazul_close.jpeg",
     openImage: "/quinceanosazul_open.jpeg",
   },
-
   {
     title: "Pink Princess",
     category: "Quince Años",
-      country: "co",
+    country: "co",
     price: "$49",
     closedImage: "/quinceanospink_close.jpeg",
     openImage: "/quinceanospink_open.jpeg",
   },
-
   {
     title: "Purple Luxury",
     category: "Quince Años",
-      country: "co",
+    country: "co",
     price: "$49",
     closedImage: "/quinceanospurpura_close.jpeg",
     openImage: "/quinceanospurpura_open.jpeg",
   },
-
   {
     title: "Royal Violet",
     category: "Quince Años",
-      country: "co",
+    country: "co",
     price: "$49",
     closedImage: "/quinceanosmorado_close.jpeg",
     openImage: "/quinceanosmorado_open.jpeg",
   },
-
   {
     title: "Neutral Floral",
     category: "Quince Años",
-      country: "co",
+    country: "co",
     price: "$49",
     closedImage: "/quinceanosfloresneutral_close.jpeg",
     openImage: "/quinceanosfloresneutral_open.jpeg",
   },
 
-  // ==========================
-  // FIESTAS INFANTILES
-  // ==========================
-
   {
     title: "Robot Party",
     category: "Cumpleaños y fiestas infantiles",
-      country: "co",
+    country: "co",
     price: "$39",
     closedImage: "/robotparty_close.jpeg",
     openImage: "/robotparty_open.jpeg",
   },
-
   {
     title: "Dinosaur Party",
     category: "Cumpleaños y fiestas infantiles",
-      country: "co",
+    country: "co",
     price: "$39",
     closedImage: "/cumpledinosaurios_close.jpeg",
     openImage: "/cumpledinosaurios_open.jpeg",
   },
-
   {
     title: "Princess Sofia",
     category: "Cumpleaños y fiestas infantiles",
-      country: "co",
+    country: "co",
     price: "$39",
     closedImage: "/cumpleprincesasofia_close.jpeg",
     openImage: "/cumpleprincesasofia_open.jpeg",
   },
-
   {
     title: "Fairy Party",
     category: "Cumpleaños y fiestas infantiles",
-      country: "co",
+    country: "co",
     price: "$39",
     closedImage: "/cumplehadas_close.jpeg",
     openImage: "/cumplehadas_open.jpeg",
   },
-
   {
     title: "Unicorn Party",
-   category: "Cumpleaños y fiestas infantiles",
-      country: "co",
-   price: "$39",
+    category: "Cumpleaños y fiestas infantiles",
+    country: "co",
+    price: "$39",
     closedImage: "/cumpleunicornios_close.jpeg",
     openImage: "/cumpleunicornios_open.jpeg",
   },
-
   {
     title: "Soccer Party",
     category: "Cumpleaños y fiestas infantiles",
-      country: "co",
+    country: "co",
     price: "$39",
     closedImage: "/cumplefutbol_close.jpeg",
     openImage: "/cumplefutbol_open.jpeg",
   },
-
   {
     title: "Cars Party",
     category: "Cumpleaños y fiestas infantiles",
-      country: "co",
+    country: "co",
     price: "$39",
     closedImage: "/cumplecars_close.jpeg",
     openImage: "/cumplecars_open.jpeg",
   },
-
   {
     title: "Animals Party",
     category: "Cumpleaños y fiestas infantiles",
-      country: "co",
+    country: "co",
     price: "$39",
     closedImage: "/cumpleanimals_close.jpeg",
     openImage: "/cumpleanimals_open.jpeg",
   },
-
   {
     title: "Basketball Party",
     category: "Cumpleaños y fiestas infantiles",
-      country: "co",
+    country: "co",
     price: "$39",
     closedImage: "/cumplebaloncesto_close.jpeg",
     openImage: "/cumplebaloncesto_open.jpeg",
   },
-
-
-
   {
     title: "Mermaid Party",
     category: "Cumpleaños y fiestas infantiles",
-      country: "co",
+    country: "co",
     price: "$39",
     closedImage: "/sirena_close.jpeg",
     openImage: "/sirena_open.jpeg",
   },
 
-  // ==========================
-  // BABY SHOWER
-  // ==========================
-
   {
     title: "Blue Teddy",
     category: "Baby Shower",
-      country: "co",
+    country: "co",
     price: "$39",
     closedImage: "/babyshowerblueoso_open.jpeg",
     openImage: "/babyshowerblueoso_open.jpeg",
   },
-
   {
     title: "Green Teddy",
     category: "Baby Shower",
-      country: "co",
+    country: "co",
     price: "$39",
     closedImage: "/babyshowergreenoso_open.jpeg",
     openImage: "/babyshowergreenoso_open.jpeg",
   },
-
   {
     title: "Pink Teddy",
     category: "Baby Shower",
-      country: "co",
+    country: "co",
     price: "$39",
     closedImage: "/babyshowerrosadooso_open.jpeg",
     openImage: "/babyshowerrosadooso_open.jpeg",
   },
-
   {
     title: "Yellow Teddy",
     category: "Baby Shower",
-      country: "co",
+    country: "co",
     price: "$39",
     closedImage: "/babyshoweryellowoso_open.jpeg",
     openImage: "/babyshoweryellowoso_open.jpeg",
   },
-
   {
     title: "Yellow Baby",
     category: "Baby Shower",
-      country: "co",
+    country: "co",
     price: "$39",
     closedImage: "/babyshower_yellow_close.jpeg",
     openImage: "/babyshower_yellow_open.jpeg",
   },
-
   {
     title: "Pink Baby",
     category: "Baby Shower",
-      country: "co",
+    country: "co",
     price: "$39",
     closedImage: "/babyshowerrosado_open.jpeg",
     openImage: "/babyshowerrosado_open.jpeg",
   },
-
   {
     title: "Green Baby",
     category: "Baby Shower",
-      country: "co",
+    country: "co",
     price: "$39",
     closedImage: "/babyshower_green_open.jpeg",
     openImage: "/babyshower_green_open.jpeg",
   },
 
-  // ==========================
-  // CELEBRACIONES
-  // ==========================
-
   {
     title: "Disco Party",
     category: "Celebraciones y fiestas",
-      country: "co",
+    country: "co",
     price: "$39",
     closedImage: "/discoparty_close.jpeg",
     openImage: "/discoparty_open.jpeg",
   },
-
   {
     title: "Spa Party",
     category: "Celebraciones y fiestas",
-      country: "co",
+    country: "co",
     price: "$39",
     closedImage: "/spaparty_close.jpeg",
     openImage: "/spaparty_open.jpeg",
   },
-
   {
     title: "Magic Party",
     category: "Celebraciones y fiestas",
-      country: "co",
+    country: "co",
     price: "$39",
     closedImage: "/fiestamagia_close.jpeg",
     openImage: "/fiestamagia_open.jpeg",
   },
-
   {
     title: "Hawaii Party",
     category: "Celebraciones y fiestas",
-      country: "co",
+    country: "co",
     price: "$39",
     closedImage: "/hawaifiesta_close.jpeg",
     openImage: "/hawaifiesta_open.jpeg",
   },
-
   {
     title: "Halloween Black",
     category: "Celebraciones y fiestas",
-      country: "co",
+    country: "co",
     price: "$39",
     closedImage: "/halloweenpartyblack_close.jpeg",
     openImage: "/halloweenpartyblack_open.jpeg",
   },
-
   {
     title: "Halloween Orange",
     category: "Celebraciones y fiestas",
-      country: "co",
+    country: "co",
     price: "$39",
     closedImage: "/halloweenparty_close.jpeg",
     openImage: "/halloweenparty_open.jpeg",
   },
-
   {
     title: "Super Party",
     category: "Celebraciones y fiestas",
-      country: "co",
+    country: "co",
     price: "$39",
     closedImage: "/superfiesta_close.jpeg",
     openImage: "/superfiesta_open.jpeg",
   },
 
+  {
+    title: "Emerald Wedding",
+    category: "Matrimonio",
+    country: "co",
+    price: "$49",
+    closedImage: "/emeraldwedding_close.jpeg",
+    openImage: "/emeraldwedding_open.jpeg",
+  },
+  {
+    title: "Classic Baptism",
+    category: "Primera comunión y bautismo",
+    country: "co",
+    price: "$39",
+    closedImage: "/bautismo_close.jpeg",
+    openImage: "/bautismo_open.jpeg",
+  },
+  {
+    title: "Corporate Event",
+    category: "Corporativo",
+    country: "co",
+    price: "$49",
+    closedImage: "/corporativo_close.jpeg",
+    openImage: "/corporativo_open.jpeg",
+  },
+  {
+    title: "Graduación Clásica",
+    category: "Graduación",
+    country: "co",
+    price: "$39",
+    closedImage: "/robotparty_close.jpeg",
+    openImage: "/robotparty_open.jpeg",
+  },
 
+  // ==========================
+  // PERÚ
+  // ==========================
 
-// ==========================
-// MATRIMONIO
-// ==========================
+  {
+    title: "Quinceañera Elegante",
+    category: "Quince Años",
+    country: "pe",
+    price: "S/ 120",
+    closedImage: "/quinceanosverde_close.jpeg",
+    openImage: "/quinceanosverde_open.jpeg",
+  },
+  {
+    title: "Boda Elegante",
+    category: "Matrimonio",
+    country: "pe",
+    price: "S/ 150",
+    closedImage: "/emeraldwedding_close.jpeg",
+    openImage: "/emeraldwedding_open.jpeg",
+  },
+  {
+    title: "Cumpleaños Infantil",
+    category: "Cumpleaños y fiestas infantiles",
+    country: "pe",
+    price: "S/ 100",
+    closedImage: "/robotparty_close.jpeg",
+    openImage: "/robotparty_open.jpeg",
+  },
+  {
+    title: "Baby Shower",
+    category: "Baby Shower",
+    country: "pe",
+    price: "S/ 100",
+    closedImage: "/babyshower_yellow_close.jpeg",
+    openImage: "/babyshower_yellow_open.jpeg",
+  },
+  {
+    title: "Graduación",
+    category: "Graduación",
+    country: "pe",
+    price: "S/ 100",
+    closedImage: "/robotparty_close.jpeg",
+    openImage: "/robotparty_open.jpeg",
+  },
+  {
+    title: "Primera Comunión",
+    category: "Primera comunión y bautismo",
+    country: "pe",
+    price: "S/ 100",
+    closedImage: "/bautismo_close.jpeg",
+    openImage: "/bautismo_open.jpeg",
+  },
 
-{
-  title: "Emerald Wedding",
-  category: "Matrimonio",
-  country: "co",
-  price: "$49",
-  closedImage: "/emeraldwedding_close.jpeg",
-  openImage: "/emeraldwedding_open.jpeg",
-},
+  // ==========================
+  // USA
+  // ==========================
 
-// ==========================
-// PRIMERA COMUNIÓN Y BAUTISMO
-// ==========================
+  {
+    title: "Emerald Wedding",
+    category: "Weddings",
+    country: "us",
+    price: "$49",
+    closedImage: "/emeraldwedding_close.jpeg",
+    openImage: "/emeraldwedding_open.jpeg",
+  },
+  {
+    title: "Birthday Party",
+    category: "Birthdays",
+    country: "us",
+    price: "$39",
+    closedImage: "/robotparty_close.jpeg",
+    openImage: "/robotparty_open.jpeg",
+  },
+  {
+    title: "Baby Shower",
+    category: "Baby Shower",
+    country: "us",
+    price: "$39",
+    closedImage: "/babyshower_yellow_close.jpeg",
+    openImage: "/babyshower_yellow_open.jpeg",
+  },
+  {
+    title: "Graduation Classic",
+    category: "Graduation",
+    country: "us",
+    price: "$39",
+    closedImage: "/robotparty_close.jpeg",
+    openImage: "/robotparty_open.jpeg",
+  },
+  {
+    title: "Corporate Event",
+    category: "Corporate",
+    country: "us",
+    price: "$49",
+    closedImage: "/corporativo_close.jpeg",
+    openImage: "/corporativo_open.jpeg",
+  },
 
-{
-  title: "Classic Baptism",
-  category: "Primera comunión y bautismo",
-  country: "co",
-  price: "$39",
-  closedImage: "/bautismo_close.jpeg",
-  openImage: "/bautismo_open.jpeg",
-},
+  // ==========================
+  // JAPÓN
+  // ==========================
 
-// ==========================
-// CORPORATIVO
-// ==========================
-
-{
-  title: "Corporate Event",
-  category: "Corporativo",
-  country: "co",
-  price: "$49",
-  closedImage: "/corporativo_close.jpeg",
-  openImage: "/corporativo_open.jpeg",
-},
-
-
-
-// ==========================
-// GRADUACIÓN
-// ==========================
-
-// Agrega aquí las invitaciones cuando las diseñemos.
-{
-  title: "Graduación Clásica",
-  category: "Graduación",
-  country: "co",
-  price: "$39",
-  closedImage: "/robotparty_close.jpeg",
-  openImage: "/robotparty_open.jpeg",
-},
+  {
+    title: "結婚式デザイン",
+    category: "結婚式",
+    country: "jp",
+    price: "¥7000",
+    closedImage: "/emeraldwedding_close.jpeg",
+    openImage: "/emeraldwedding_open.jpeg",
+  },
+  {
+    title: "誕生日デザイン",
+    category: "誕生日",
+    country: "jp",
+    price: "¥6000",
+    closedImage: "/robotparty_close.jpeg",
+    openImage: "/robotparty_open.jpeg",
+  },
+  {
+    title: "ベビーシャワー",
+    category: "ベビーシャワー",
+    country: "jp",
+    price: "¥6000",
+    closedImage: "/babyshower_yellow_close.jpeg",
+    openImage: "/babyshower_yellow_open.jpeg",
+  },
+  {
+    title: "卒業デザイン",
+    category: "卒業",
+    country: "jp",
+    price: "¥6000",
+    closedImage: "/robotparty_close.jpeg",
+    openImage: "/robotparty_open.jpeg",
+  },
+  {
+    title: "法人イベント",
+    category: "法人イベント",
+    country: "jp",
+    price: "¥7000",
+    closedImage: "/corporativo_close.jpeg",
+    openImage: "/corporativo_open.jpeg",
+  },
 ];
