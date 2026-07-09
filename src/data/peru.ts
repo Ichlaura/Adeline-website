5d5f457fa1fd02c26274b274a637b4c267bbe5fa
laura nunez <laupao41@gmail.com>
2026-07-09 16:25:06 +0900

    correciones 3

--- src/data/peru.ts
+++ src/data/peru.ts
@@ -8,24 +8,17 @@ export type Invitation = {
   closedImage: string;
   openImage: string;
 };
-
 export const peruInvitations: Invitation[] = [
-
-  // ==========================
-  // PERÚ
-  // ==========================
-
   {
-    title: "Quinceañera Elegante",
+    title: "Emerald Quinceañera",
     category: "Quince Años",
     country: "pe",
     price: "S/ 120",
     closedImage: "/quinceanosverde_close.jpeg",
     openImage: "/quinceanosverde_open.jpeg",
   },
-  
   {
-    title: "Cumpleaños Infantil",
+    title: "Robot Party",
     category: "Cumpleaños y fiestas infantiles",
     country: "pe",
     price: "S/ 100",
@@ -33,7 +26,7 @@ export const peruInvitations: Invitation[] = [
     openImage: "/robotparty_open.jpeg",
   },
   {
-    title: "Baby Shower",
+    title: "Yellow Baby",
     category: "Baby Shower",
     country: "pe",
     price: "S/ 100",
@@ -41,109 +34,109 @@ export const peruInvitations: Invitation[] = [
     openImage: "/babyshower_yellow_open.jpeg",
   },
   {
-  title: "Revelación de género",
-  category: "Revelación de género",
-  country: "co",
-  price: "$39",
-  closedImage: "/babyshower_yellow_close.jpeg",
-  openImage: "/babyshower_yellow_open.jpeg",
-},
+    title: "Revelación de género",
+    category: "Revelación de género",
+    country: "pe",
+    price: "S/ 100",
+    closedImage: "/babyshower_yellow_close.jpeg",
+    openImage: "/babyshower_yellow_open.jpeg",
+  },
   {
-    title: "Graduación",
+    title: "Graduación Clásica",
     category: "Graduación",
     country: "pe",
     price: "S/ 100",
     closedImage: "/robotparty_close.jpeg",
     openImage: "/robotparty_open.jpeg",
   },
-  
+
   {
     title: "Pink Blessing",
     category: "Primera comunión y bautismo",
     country: "pe",
-    price: "$39",
-    closedImage: "/PinkBlessing.close.png",
-    openImage: "/PinkBlessing.open.png",
+    price: "S/ 100",
+    closedImage: "/Pink Blessing.close.png",
+    openImage: "/Pink Blessing.open.png",
   },
-
   {
     title: "Golden Blessing",
     category: "Primera comunión y bautismo",
     country: "pe",
-    price: "$39",
-    closedImage: "/GoldenBlessing.close.png",
-    openImage: "/GoldenBlessing.open.png",
+    price: "S/ 100",
+    closedImage: "/Golden Blessing.close.png",
+    openImage: "/Golden Blessing.open.png",
   },
-
   {
     title: "Blue Blessing",
     category: "Primera comunión y bautismo",
     country: "pe",
-    price: "$39",
-    closedImage: "/BlueBlessing.close.png",
-    openImage: "/BlueBlessing.open.png",
+    price: "S/ 100",
+    closedImage: "/Blue Blessing.close.png",
+    openImage: "/Blue Blessing.open.png",
   },
-
   {
     title: "Beige Communion",
     category: "Primera comunión y bautismo",
     country: "pe",
-    price: "$39",
-    closedImage: "/BeigeCommunion.close.png",
-    openImage: "/BeigeCommunion.open.png",
+    price: "S/ 100",
+    closedImage: "/Beige Communion.close.png",
+    openImage: "/Beige Communion.open.png",
   },
 
   {
     title: "Gold Wedding",
     category: "Matrimonio",
     country: "pe",
-    price: "$39",
-    closedImage: "/GoldWedding.close.png",
-    openImage: "/GoldWedding.open.png",
-   },
-
+    price: "S/ 120",
+    closedImage: "/Gold Wedding.close.png",
+    openImage: "/Gold Wedding.open.png",
+  },
   {
     title: "Blue Wedding",
     category: "Matrimonio",
     country: "pe",
-    price: "$39",
-    closedImage: "/BlueWedding.close.png",
-    openImage: "/BlueWedding.open.png",
+    price: "S/ 120",
+    closedImage: "/Blue.Wedding.close.png",
+    openImage: "/Blue.Wedding.open.png",
+  },
+  {
+    title: "Royal Emerald",
+    category: "Matrimonio",
+    country: "pe",
+    price: "S/ 120",
+    closedImage: "/Royal Emerald.close.png",
+    openImage: "/Royal Emerald.open.png",
   },
   {
     title: "Champagne Wedding",
     category: "Matrimonio",
     country: "pe",
-    price: "$39",
-    closedImage: "/ChampagneWedding.close.png",
-    openImage: "/ChampagneWedding.open.png",
+    price: "S/ 120",
+    closedImage: "/Champagne.Wedding.close.png",
+    openImage: "/Champagne.Wedding.open.png",
   },
-
   {
     title: "Pearl Wedding",
     category: "Matrimonio",
     country: "pe",
-    price: "$39",
-    closedImage: "/PearlWedding.close.png",
-    openImage: "/PearlWedding.open.png",
+    price: "S/ 120",
+    closedImage: "/Pearl Wedding.close.png",
+    openImage: "/Pearl Wedding.open.png",
   },
-
   {
     title: "Silver Wedding",
     category: "Matrimonio",
     country: "pe",
-    price: "$39",
-    closedImage: "/SilverWedding.close.png",
-    openImage: "/SilverWedding.open.png",
+    price: "S/ 120",
+    closedImage: "/Silver.Wedding.close.png",
+    openImage: "/Silver.Wedding.open.png",
   },
-
   {
     title: "Green Wedding",
     category: "Matrimonio",
     country: "pe",
-    price: "$39",
-    closedImage: "/GreenWedding.close.png",
-    openImage: "/GreenWedding.open.png",
+    price: "S/ 120",
+    closedImage: "/Green Wedding.close.png",
+    openImage: "/Green Wedding.open.png",
   },
-
 ];
